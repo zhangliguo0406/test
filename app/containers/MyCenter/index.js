9a02963e8d4560063b3e559c90a8be72c86b8f67
zhangliguo0406 <397714759@163.com>
2017-08-08 09:54:59 +0800

    框架初步完成

---
+++ app/containers/MyCenter/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by z on 2017/8/7.
+ */
+import React,{Component} from 'react';
+export default class MyCenter extends Component{
+    render(){
+        return(
+            <div>
+               我的个人信息
+            </div>
+        )
+    }
+}
---
+++ app/containers/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by z on 2017/8/7.
+ */
+import React,{Component} from 'react';
+import RouterMap from "../routers/index";
+export default class App extends Component{
+    render(){
+        return(
+            <div>
+                <RouterMap/>
+            </div>
+        )
+    }
+}
